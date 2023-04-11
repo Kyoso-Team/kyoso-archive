e8a877c3d4e67f78d08c559d4de35d916ac74d49
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-04-11 23:20:33 +0200

    Change how URLs of uploaded files work

--- src/lib/trpc/routes/upload.ts
+++ src/lib/trpc/routes/upload.ts
@@ -1,6 +1,5 @@
 import { t } from '$trpc';
 import { getUploadInfo, getUser } from '$trpc/middleware';
-import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import env from '$lib/env/server';
 
@@ -21,7 +20,6 @@ export const uploadRouter = t.router({
     }
   }),
 
-  
   upload: t.procedure
   .use(getUser)
   .use(getUploadInfo)
@@ -52,7 +50,17 @@ export const uploadRouter = t.router({
   .mutation(async ({ ctx }) => {
     if (!ctx.allowed) {return false}
     let info = ctx.uploadInfo
-    let upload = await fetch(`${env.STORAGE_ENDPOINT}/${info.uploadType}_${info.targetId}/${info.upload.name}`, {
+    
+    /**
+     * File name is the `targetId` + an appropriate extension
+     * The extension does not relate to what the file actually is,
+     * rather what it's supposed to be according to `uploadType`
+     * (at least, until implementation of file type checking)
+     */
+    let extension = info.uploadType !== "replay" ? ".jpg" : ".osr"
+    let name = `${info.targetId}${extension}`
+
+    let upload = await fetch(`${env.STORAGE_ENDPOINT}/${info.uploadType}/${name}`, {
       method: "PUT",
       headers: {
         AccessKey: env.STORAGE_PASSWORD,
@@ -64,43 +72,10 @@ export const uploadRouter = t.router({
     if (!upload.ok) {
       return null
     } else {
-      removeOldFiles(`${env.STORAGE_ENDPOINT}/${info.uploadType}_${info.targetId}/`, info.upload.name)
       return {
-        url: `/uploads/${info.uploadType}_${info.targetId}/${info.upload.name}`,
+        url: `/uploads/${info.uploadType}/${name}`,
         file: info.upload
       }
     }
   })
 })
-
-/**
- * When a user uploads a file, it may render some previously uploaded file outdated or meaningless
- * To not store files for no reason, we delete those files!
- * It is done through that function for syncronisation reasons
- * (there's no need to wait for the removal to be done to send back the file info to the uploader)
- */
-async function removeOldFiles(fetchUrl: string, newerFileName: string) {
-  let list_response = await fetch(fetchUrl, {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      AccessKey: env.STORAGE_PASSWORD
-    }
-  })
-  let list = await list_response.json()
-
-  for (let i = 0; i < list.length; i++) {
-    if (list[i]["ObjectName"] !== newerFileName) {
-      let deletion = await fetch(`${fetchUrl}${list[i]["ObjectName"]}`, {
-        method: "DELETE",
-        headers: {
-          AccessKey: env.STORAGE_PASSWORD
-        }
-      })
-      
-      if (!deletion.ok) {
-        console.error("Failed to delete a file from storage", list[i], deletion)
-      }
-    }
-  }
-}
