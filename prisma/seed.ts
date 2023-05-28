5bd662672ade87683e4b799f38bef84ff4380d84
L-Mario564 <ka.mario564@gmail.com>
2023-05-28 15:30:06 -0600

    Fetch real user data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,89 +1,152 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import fetch from 'node-fetch';
+import { PrismaClient } from '@prisma/client';
+import { Client as OsuClient, Auth as OsuAuth } from 'osu-web.js';
+import { config } from 'dotenv';
+import { z } from 'zod';
+import type { User as DiscordUser } from 'discord-oauth2';
 
-function isUnique(result: any, uniqueArr: {[index: string]:any}[], uniqueKey: string): boolean {
-	for (let i = 0; i < uniqueArr.length; i++) {
-		if (uniqueArr[i][uniqueKey] === result) {
-			return false
-		}
-	}
-	return true
-}
-function rBool(): boolean {
-	return Math.random() < 0.5
-}
-function rString(min: number, max: number = min, uniqueArr?: {[index: string]:any}[], uniqueKey?: string): string {
-	let length = rNumber(min, max)
-	let result = [...Array(length)].map(() => (Math.random() || 0.69).toString(36)[2]).join('')
+config();
 
-	if ((!uniqueArr || !uniqueKey) || isUnique(result, uniqueArr, uniqueKey)) {
-		return result
-	} else {
-		return rString(min, max, uniqueArr, uniqueKey)
-	}
-}
-function rNumber(min: number, max: number = min, uniqueArr?: {[index: string]:any}[], uniqueKey?: string): number {
-	let result = Math.floor(Math.random() * (max + 1 - min) + min)
+let env = z.object({
+  osuClientId: z.number(),
+  osuClientSecret: z.string(),
+  osuRedirectUri: z.string(),
+  discordBotToken: z.string()
+}).parse({
+  osuClientId: Number(process.env.PUBLIC_OSU_CLIENT_ID),
+  osuClientSecret: process.env.OSU_CLIENT_SECRET,
+  osuRedirectUri: process.env.PUBLIC_OSU_REDIRECT_URI,
+  discordBotToken: process.env.DISCORD_BOT_TOKEN
+});
 
-	if ((!uniqueArr || !uniqueKey) || isUnique(result, uniqueArr, uniqueKey)) {
-		return result
-	} else {
-		return rNumber(min, max, uniqueArr, uniqueKey)
-	}
-}
+const prisma = new PrismaClient();
 
-async function createUsers(number: number): Promise<"done"> {
-	let users: Prisma.UserCreateInput[] = []
-	
-	for (let i = 0; i < number; i++) {
-		let countryCode = rString(2).toUpperCase()
-		users.push({
-			isAdmin: rBool(),
-			osuUserId: rNumber(1, 99999999, users, "osuUserId"),
-			isRestricted: rBool(),
-			osuUsername: rString(3, 16, users, "osuUsername"),
-			discordUserId: rString(3, 19, users, "discordUserId"),
-			discordUsername: rString(3, 32),
-			discordDiscriminator: rNumber(1, 9999),
-			apiKey: rString(24),
-			osuAccessToken: rString(64),
-			osuRefreshToken: rString(64),
-			discordAccesstoken: rString(64),
-			discordRefreshToken: rString(64),
-			country: {
-				connectOrCreate: {
-					create: {
-						code: countryCode,
-						name: rString(6, 24)
-					},
-					where: {
-						code: countryCode
-					}
-				}
-			}
-		})
-	}
+const osuAuth = new OsuAuth(env.osuClientId, env.osuClientSecret, env.osuRedirectUri);
+const osuToken = await osuAuth.clientCredentialsGrant();
+const osuClient = new OsuClient(osuToken.access_token);
 
-	for (let i = 0; i < users.length; i++) {
-		await prisma.user.create({data: users[i]})
-		process.stdout.write(".")
-	}
+/**
+ * Randomize values
+ */
+const r = {
+  boolean: () => {
+    return Math.random() < 0.5;
+  },
+  string: (min: number, max: number = min) => {
+    let length = r.number(min, max);
+    return [...Array(length)].map(() => (Math.random() || 0.69).toString(36)[2]).join('');
+  },
+  number: (min: number, max: number = min) => {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+  }
+};
 
-	return "done"
+async function seedUsers(): Promise<'done'> {
+  function id(osu: number, discord: string) {
+    return { osu, discord };
+  }
+
+  let ids = [
+    id(8203119, '443192613621858304'),
+    id(9676089, '321933284114300928'),
+    id(6231292, '308083236071014402'),
+    id(12058601, '194198021829951489'),
+    id(1797189, '125419051290853376'),
+    id(15173398, '336333475814703104'),
+    id(14112951, '331031326688018432'),
+    id(7279762, '196497984580616193'),
+    id(10000899, '250693235091963904'),
+    id(2756335, '369469339083997185'),
+    id(1576095, '823764250304446504'),
+    id(9918921, '296804750295302145'),
+    id(808976, '425247127346741250'),
+    id(13352562, '842179503408414781'),
+    id(14271017, '201007982060765184'),
+    id(8599070, '126806732889522176'),
+    id(3958619, '185648818111512576'),
+    id(20971891, '452410682214842368'),
+    id(12455868, '345003515594276865'),
+    id(16060432, '345670350773813250')
+  ];
+  
+  await Promise.all(ids.map(async (id) => {
+    let osu = await osuClient.users.getUser(id.osu, {
+      query: {
+        key: 'id'
+      },
+      urlParams: {
+        mode: 'osu'
+      }
+    });
+
+    let discordResp = await fetch(`https://discord.com/api/users/${id.discord}`, {
+      headers: {
+        Authorization: `Bot ${env.discordBotToken}`
+      }
+    });
+    let discord = await discordResp.json() as DiscordUser;
+
+    await prisma.user.create({
+      data: {
+        apiKey: r.string(24),
+        discordAccesstoken: r.string(64),
+        discordRefreshToken: r.string(64),
+        osuAccessToken: r.string(64),
+        osuRefreshToken: r.string(64),
+        isAdmin: r.boolean(),
+        isRestricted: r.boolean(),
+        osuUserId: osu.id,
+        osuUsername: osu.username,
+        discordUserId: discord.id,
+        discordUsername: discord.username,
+        discordDiscriminator: discord.discriminator,
+        country: {
+          connectOrCreate: {
+            create: {
+              code: osu.country.code,
+              name: osu.country.name
+            },
+            where: {
+              code: osu.country.code
+            }
+          }
+        }
+      }
+    });
+  }));
+
+  return 'done';
 }
 
 async function main() {
-	console.log("Users: ")
-	console.log(await createUsers(50))
-}
+  // Deletes all rows in the database (https://dba.stackexchange.com/questions/154061/delete-all-data-in-postgres-database)
+  await prisma.$queryRaw`
+    DO
+    $$
+    DECLARE
+      l_stmt text;
+    BEGIN
+      SELECT 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',')
+        INTO l_stmt
+      FROM pg_tables
+      WHERE schemaname IN ('public');
 
+      EXECUTE l_stmt;
+    END;
+    $$
+  `;
+
+
+  console.log('Users: ');
+  console.log(await seedUsers());
+}
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
