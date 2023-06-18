67d512597e0e32777aa2b5b0f8a565e205dca7d2
L-Mario564 <ka.mario564@gmail.com>
2023-06-17 20:16:15 -0600

    Updated seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,6 +27,8 @@ const osuAuth = new OsuAuth(env.osuClientId, env.osuClientSecret, env.osuRedirec
 const osuToken = await osuAuth.clientCredentialsGrant();
 const osuClient = new OsuClient(osuToken.access_token);
 
+const userCount = 50;
+
 /**
  * Randomize values
  */
@@ -38,10 +40,13 @@ const r = {
     let length = r.number(min, max);
     return [...Array(length)].map(() => (Math.random() || 0.69).toString(36)[2]).join('');
   },
-  number: (min: number, max: number = min) => {
-    return Math.floor(Math.random() * (max + 1 - min) + min);
+  number: (min: number, max: number = min, exclude: number[] = []): number => {
+    let result = Math.floor(Math.random() * (max + 1 - min) + min);
+    return exclude.find((value) => value === result)
+    ? r.number(min, max, exclude)
+    : result;
   },
-  array: (arr: any[], amount?: number) => {
+  array: <T>(arr: T[], amount?: number) => {
     let x = arr.length;
     for (let i = 0; amount ? arr.length === amount : i < x; i++) {
       if (r.boolean() && arr.length > 1) {
@@ -52,52 +57,100 @@ const r = {
   }
 };
 
-async function seedUsers(): Promise<'done'> {
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function seedUsers() {
   function id(osu: number, discord: string) {
     return { osu, discord };
   }
 
+  // Feel free to add more users
+  // If you do so, change the userCount constant to match the amount of users below
+  // A 2D array consisting of 10 IDs per array is made to avoid making too many calls to the osu! and Discord API
   let ids = [
-    id(8203119, '443192613621858304'),
-    id(9676089, '321933284114300928'),
-    id(6231292, '308083236071014402'),
-    id(12058601, '194198021829951489'),
-    id(1797189, '125419051290853376'),
-    id(15173398, '336333475814703104'),
-    id(14112951, '331031326688018432'),
-    id(7279762, '196497984580616193'),
-    id(10000899, '250693235091963904'),
-    id(2756335, '369469339083997185'),
-    id(1576095, '823764250304446504'),
-    id(9918921, '296804750295302145'),
-    id(808976, '425247127346741250'),
-    id(13352562, '842179503408414781'),
-    id(14271017, '201007982060765184'),
-    id(8599070, '126806732889522176'),
-    id(3958619, '185648818111512576'),
-    id(20971891, '452410682214842368'),
-    id(12455868, '345003515594276865'),
-    id(16060432, '345670350773813250')
+    [
+      id(8203119, '443192613621858304'),
+      id(9676089, '321933284114300928'),
+      id(6231292, '308083236071014402'),
+      id(12058601, '194198021829951489'),
+      id(1797189, '125419051290853376'),
+      id(15173398, '336333475814703104'),
+      id(14112951, '331031326688018432'),
+      id(7279762, '196497984580616193'),
+      id(10000899, '250693235091963904'),
+      id(2756335, '369469339083997185')
+    ],
+    [
+      id(1576095, '823764250304446504'),
+      id(9918921, '296804750295302145'),
+      id(808976, '425247127346741250'),
+      id(13352562, '842179503408414781'),
+      id(14271017, '201007982060765184'),
+      id(8599070, '126806732889522176'),
+      id(3958619, '185648818111512576'),
+      id(20971891, '452410682214842368'),
+      id(12455868, '345003515594276865'),
+      id(7892873, '203153128940371968')
+    ],
+    [
+      id(17980099, '244886511370240001'),
+      id(21728944, '248893859126312960'),
+      id(14356517, '621923547798962177'),
+      id(9433144, '177799252951564288'),
+      id(15229290, '587825148267331594'),
+      id(3372322, '121310011778531331'),
+      id(18618027, '311157082973798400'),
+      id(1013605, '556857604060479489'),
+      id(8240995, '142725947760574464'),
+      id(13968504, '671166695116898314')
+    ],
+    [
+      id(6968364, '134285412498669568'),
+      id(25512851, '606853872413048852'),
+      id(16834515, '544302017376550923'),
+      id(18365259, '846755445745123338'),
+      id(15899307, '381439606010150913'),
+      id(7671790, '150313781673721856'),
+      id(9188548, '108904078351814656'),
+      id(16060432, '345670350773813250'),
+      id(1997719, '121440769331560459'),
+      id(18315188, '133345027660316673')
+    ],
+    [
+      id(12835025, '332601750169321483'),
+      id(9808432, '238751195345190913'),
+      id(16052525, '197089152074907648'),
+      id(8406711, '274919681809186817'),
+      id(11364009, '330540904349696000'),
+      id(3191010, '258709654366978049'),
+      id(17100083, '351147827289653249'),
+      id(12754827, '346046056703721482'),
+      id(9501251, '140893290647126017'),
+      id(13684009, '183552082681266176')
+    ]
   ];
 
-  await Promise.all(
-    ids.map(async (id) => {
-      let osu = await osuClient.users.getUser(id.osu, {
-        query: {
-          key: 'id'
-        },
-        urlParams: {
-          mode: 'osu'
-        }
-      });
-
+  for (let i = 0; i < ids.length; i++) {
+    await Promise.all(
+      ids[i].map(async (id) => {
+        let osu = await osuClient.users.getUser(id.osu, {
+          query: {
+            key: 'id'
+          },
+          urlParams: {
+            mode: 'osu'
+          }
+        });
+  
       let discordResp = await fetch(`https://discord.com/api/users/${id.discord}`, {
         headers: {
           Authorization: `Bot ${env.discordBotToken}`
         }
       });
       let discord = (await discordResp.json()) as DiscordUser;
-
+    
       await prisma.user.create({
         data: {
           apiKey: r.string(24),
@@ -123,130 +176,182 @@ async function seedUsers(): Promise<'done'> {
               }
             }
           }
-        }
-      });
-    })
-  );
+        },
+        select: {
+          id: true
+          }
+        });
+      })
+    );
 
-  return 'done';
+    await sleep(1000);
+  }
+
+  return 'Seeded users';
 }
 
-async function seedTournaments(): Promise<'done'> {
-  for (let i = 0; i < 15; i++) {
-    let type = r.array(['Teams', 'Solo', 'Draft'], 1)[0];
-    let rankRange: 'open rank' | { lower: number; upper: number } = r.boolean()
-      ? 'open rank'
-      : {
-          lower: r.number(1, 500000),
-          upper: r.number(1, 500000)
-        };
+async function seedteamTournament() {
+  let isOpenRank = r.boolean();
 
-    await prisma.tournament.create({
+  let tournament = await prisma.tournament.create({
+    data: {
+      name: 'Test Team Tournament',
+      acronym: 'TTT',
+      useBWS: r.boolean(),
+      services: ['Registrations', 'Referee', 'Stats', 'Pickems'],
+      lowerRankRange: isOpenRank ? -1 : 1,
+      upperRankRange: isOpenRank ? -1 : 10000,
+      type: 'Teams',
+      teamSize: 3,
+      teamPlaySize: 2
+    },
+    select: {
+      id: true
+    }
+  });
+
+  let host = await prisma.staffMember.create({
+    data: {
+      userId: r.number(1, userCount),
+      tournamentId: tournament.id
+    },
+    select: {
+      id: true
+    }
+  });
+
+  let usedUserIds = [host.id];
+  let staffMemberIds: number[] = [];
+
+  for (let i = 0; i < 5; i++) {
+    let userId = r.number(1, userCount, usedUserIds);
+    usedUserIds.push(userId);
+
+    let staffMember = await prisma.staffMember.create({
       data: {
-        name: r.string(5, 50),
-        acronym: r.string(1, 8),
-        useBWS: r.boolean(),
-        services: r.array(['Registrations', 'Mappooling', 'Referee', 'Stats', 'Pickems']),
-        type,
-        lowerRankRange: rankRange === 'open rank' ? -1 : rankRange.lower,
-        upperRankRange: rankRange === 'open rank' ? -1 : rankRange.upper,
-        teamSize: type === 'Teams' ? r.number(1, 8) : 1,
-        teamPlaySize: type === 'Teams' ? r.number(1, 4) : 1,
-        inPurchases: undefined
+        userId,
+        tournamentId: tournament.id
       },
       select: {
         id: true
       }
     });
+
+    staffMemberIds.push(staffMember.id);
   }
 
-  return 'done';
-}
-
-async function seedStaffMembers(): Promise<'done'> {
-  let tournaments = await prisma.tournament.findMany({
-    select: {
-      id: true
-    }
-  });
-  let users = await prisma.user.findMany({
-    select: {
-      id: true
-    }
-  });
-
-  for (let i = 0; i < tournaments.length; i++) {
-    let staffMembers: { id: number }[] = [];
-    for (let e = 0; e < users.length; e++) {
-      if (r.boolean()) {
-        staffMembers.push(
-          await prisma.staffMember.create({
-            data: {
-              userId: users[e].id,
-              tournamentId: tournaments[i].id
-            },
-            select: {
-              id: true
-            }
-          })
-        );
+  await prisma.staffRole.create({
+    data: {
+      name: 'Host',
+      color: 'Red',
+      permissions: ['Host'],
+      order: 1,
+      tournamentId: tournament.id,
+      staffMembers: {
+        connect: {
+          id: host.id
+        }
       }
+    },
+    select: {
+      id: true
     }
+  });
 
-    for (let e = 0; e < 10; e++) {
-      await prisma.staffRole.create({
-        data: {
-          name: `${e}_${r.string(1, 23)}`,
-          color: 'Blue',
-          permissions: r.array([
-            'MutateTournament',
-            'MutateStaffMembers',
-            'MutateRegs',
-            'MutatePoolStructure',
-            'MutatePoolSuggestions',
-            'MutateMapsToPlaytest',
-            'MutateMatches',
-            'MutateStats',
-            'CanPlay'
-          ]),
-          tournamentId: tournaments[i].id,
-          staffMembers: {
-            connect: staffMembers.filter(() => r.boolean())
+  for (let i = 0; i < staffMemberIds.length; i++) {
+    await prisma.staffRole.upsert({
+      create: {
+        name: 'Referee',
+        color: 'Emerald',
+        permissions: ['RefMatches'],
+        order: 2,
+        tournamentId: tournament.id,
+        staffMembers: {
+          connect: {
+            id: staffMemberIds[i]
           }
         }
-      });
+      },
+      update: {
+        staffMembers: {
+          connect: {
+            id: staffMemberIds[i]
+          }
+        }
+      },
+      where: {
+        name_tournamentId: {
+          name: 'Referee',
+          tournamentId: tournament.id
+        }
+      }
+    });
+  }
+  
+  await prisma.modMultiplier.create({
+    data: {
+      value: 2.2,
+      mods: ['EZ'],
+      tournamentId: tournament.id
     }
+  });
+
+  let playerIds: number[] = [];
+  let randomAvailabilities = [
+    '001101110000110111000000.001101110000110111000000.001101110000110111000000.001101110000110111000000',
+    '101010100101010100010010.010101001010100101010100.010101001010100101000100.010101001010101001001010',
+    '000011100101101010101001.000111110011001011101110.100100000111000001000010.100010000100011001101001'
+  ];
+
+  for (let i = 0; i < 12; i++) {
+    let userId = r.number(1, userCount, usedUserIds);
+    usedUserIds.push(userId);
+
+
+    let player = await prisma.player.create({
+      data: {
+        userId,
+        availability: r.array(randomAvailabilities, 1)[0],
+        tournamentId: tournament.id
+      },
+      select: {
+        id: true
+      }
+    });
+
+    playerIds.push(player.id);
   }
 
-  return 'done';
+  // Todo: Create 2 teams of 2 and 2 teams of 3, only creating 1 team for now
+  await prisma.team.create({
+    data: {
+      inviteId: r.string(8),
+      name: r.string(5, 20),
+      tournamentId: tournament.id,
+      captainId: playerIds[0],
+      players: {
+        connect: [{
+          id: playerIds[0]
+        }, {
+          id: playerIds[1]
+        }, {
+          id: playerIds[2]
+        }]
+      }
+    },
+    select: {
+      id: true
+    }
+  });
+
+  return 'Seeded team tournament';
 }
 
 async function main() {
-  // Deletes all rows in the database (https://dba.stackexchange.com/questions/154061/delete-all-data-in-postgres-database)
-  await prisma.$queryRaw`
-    DO
-    $$
-    DECLARE
-      l_stmt text;
-    BEGIN
-      SELECT 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',')
-        INTO l_stmt
-      FROM pg_tables
-      WHERE schemaname IN ('public');
+  console.log('Seeding database...');
 
-      EXECUTE l_stmt;
-    END;
-    $$
-  `;
-
-  console.log('Users: ');
   console.log(await seedUsers());
-
-  console.log('Tournaments: ');
-  console.log(await seedTournaments());
-
-  console.log('Staff of tournaments: ');
-  console.log(await seedStaffMembers());
+  console.log(await seedteamTournament());
 }
 
 main()
