8a245c5994ce5fd6682fe848e728fe6adc605757
L-Mario564 <ka.mario564@gmail.com>
2023-08-06 09:42:04 -0600

    Update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,32 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
-    'prettier'
-  ],
-  plugins: ['@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser'
-      }
-    }
-  ],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
   rules: {
     'quotes': [
       'warn',
@@ -36,14 +45,5 @@ module.exports = {
     'quote-props': ['warn', 'consistent'],
     '@typescript-eslint/no-unused-vars': 'warn',
     'unused-export-let': 'off'
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
   }
 };
