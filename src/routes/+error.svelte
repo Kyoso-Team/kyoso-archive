924a074f169571a4a98a5894b17d49b36816dfdf
Mario564 <ka.mario564@gmail.com>
2024-03-09 17:05:32 -0800

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="w-screen h-full flex flex-col text-center justify-center items-center">
+  <h1 class="text-primary-500">{$page.status}</h1>
+  <p class="font-medium">{$page.error?.message}</p>
+</div>
