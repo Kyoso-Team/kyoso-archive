d02aa0099033482caac3f4752f8ed167f723fe40
Mario564 <ka.mario564@gmail.com>
2024-03-10 19:06:04 -0700

    Update tRPC global context

--- src/lib/server/trpc/context.ts
+++ src/lib/server/trpc/context.ts
@@ -2,16 +2,20 @@ import type { RequestEvent } from '@sveltejs/kit';
 import type { inferAsyncReturnType } from '@trpc/server';
 
 export async function createContext({ request, cookies, fetch, url }: RequestEvent) {
-  cookies.delete('session', {
-    path: '/trpc'
-  });
+  const sessionCookie = (request.headers.get('cookie') || '');
+
+  if (sessionCookie.match(/session/g)?.length || 0 > 1) {
+    cookies.delete('session', {
+      path: '/trpc'
+    });
+  }
 
   return {
     request,
     fetch,
     url,
     cookies: {
-      session: (request.headers.get('cookie') || '')
+      session: sessionCookie
         .split(';')
         .map((subStr) => subStr.split('=')[1])
         .find((value) => value !== '')
