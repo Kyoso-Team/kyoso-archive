314cf2404cd5628635b91ce3f6bef11f0705c26a
Mario564 <ka.mario564@gmail.com>
2024-03-16 14:44:09 -0700

    Remove unused icons

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,11 +1,6 @@
-import PickemsIcon from './Pickems.svelte';
-import PoolingIcon from './Pooling.svelte';
-import RefereeIcon from './Referee.svelte';
-import RegistrationsIcon from './Registrations.svelte';
-import StatsIcon from './Stats.svelte';
 import Osu from './Osu.svelte';
 import Discord from './Discord.svelte';
 import KyosoHybrid from './KyosoHybrid.svelte';
 import Kyoso from './Kyoso.svelte';
 
-export { PickemsIcon, PoolingIcon, RefereeIcon, RegistrationsIcon, StatsIcon, Osu, Discord, KyosoHybrid, Kyoso };
+export { Osu, Discord, KyosoHybrid, Kyoso };
