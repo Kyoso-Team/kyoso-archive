3ca33bb881ffc40c7062ef286cd89ee96b7d0098
Mario564 <ka.mario564@gmail.com>
2024-04-05 13:56:49 -0700

    Make changes to modal and tooltip

--- src/lib/components/general/Tooltip.svelte
+++ src/lib/components/general/Tooltip.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+  export let show = true;
   export let target: string;
   export let label: string;
   export let arrowBorders = 'border-b border-r';
 </script>
 
-<div
-  class="dark:bg-zinc-800 dark:text-white dark:border-zinc-200/10 border whitespace-nowrap p-2 text-center text-xs shadow-md max-w-48 text-wrap z-30"
-  data-popup={target}
->
-  {label}
-  <div class={`arrow dark:bg-zinc-800 dark:border-zinc-200/10 shadow-md ${arrowBorders}`} />
+<div data-popup={target}>
+  <div class={`dark:bg-zinc-800 break-words dark:text-white dark:border-zinc-200/10 border whitespace-nowrap p-2 text-center text-xs shadow-md max-w-48 text-wrap z-30 ${show ? 'block' : 'hidden'}`}>
+    {label}
+    <div class={`arrow dark:bg-zinc-800 dark:border-zinc-200/10 shadow-md ${arrowBorders}`} />
+  </div>
 </div>
