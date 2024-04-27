2a2b5bb01da958433177f1b7ad2fde5a8dd12cc6
Mario564 <ka.mario564@gmail.com>
2024-04-27 10:54:28 -0700

    Update get notifications function

--- src/lib/server/procedures/notifications.ts
+++ src/lib/server/procedures/notifications.ts
@@ -1,11 +1,109 @@
 import * as v from 'valibot';
-import { db, UserNotification } from '$db';
+import { db, Notification, OsuUser, Tournament, User } from '$db';
 import { t } from '$trpc';
-import { trpcUnknownError } from '$lib/server/utils';
+import { pick, trpcUnknownError } from '$lib/server/utils';
 import { wrap } from '@typeschema/valibot';
 import { getSession } from '../helpers/trpc';
-import { and, eq } from 'drizzle-orm';
+import { and, desc, eq, inArray } from 'drizzle-orm';
 import { positiveIntSchema } from '$lib/schemas';
+
+const getNotifications = t.procedure
+  .input(
+    wrap(
+      v.object({
+        pagination: v.object({
+          limit: positiveIntSchema,
+          offset: positiveIntSchema
+        })
+      })
+    )
+  )
+  .query(async ({ ctx, input }) => {
+    const { pagination } = input;
+    const session = getSession(ctx.cookies, true);
+
+    let notifications: Pick<typeof Notification.$inferSelect, 'notifiedAt' | 'message' | 'read'>[] =
+      [];
+
+    try {
+      notifications = await db
+        .select({
+          notifiedAt: Notification.notifiedAt,
+          read: Notification.read,
+          message: Notification.message
+        })
+        .from(Notification)
+        .where(eq(Notification.userId, session.userId))
+        .orderBy(desc(Notification.notifiedAt))
+        .limit(pagination.limit)
+        .offset(pagination.offset);
+    } catch (err) {
+      throw trpcUnknownError(err, 'Getting the notifications');
+    }
+
+    const messageVars = Array.from(
+      new Set(notifications.map(({ message }) => message.match(/(\w+):(\w+)/g) || []).flat())
+    );
+
+    const tournamentsToGet: number[] = [];
+    const usersToGet: number[] = [];
+
+    let tournaments: Pick<typeof Tournament.$inferSelect, 'name' | 'urlSlug'>[] = [];
+    let users: (Pick<typeof User.$inferSelect, 'id'> &
+      Pick<typeof OsuUser.$inferSelect, 'username'>)[] = [];
+
+    for (let i = 0; i < messageVars.length; i++) {
+      const split = messageVars[i].split(':');
+      const thing = split[0];
+      const id = split[1];
+
+      switch (thing) {
+        case 'tournament':
+          tournamentsToGet.push(Number(id));
+          break;
+        case 'user':
+          usersToGet.push(Number(id));
+          break;
+        default:
+          console.warn('Unknown variable in notification message');
+          break;
+      }
+    }
+
+    if (tournamentsToGet.length > 0) {
+      try {
+        tournaments = await db
+          .select(pick(Tournament, ['name', 'urlSlug']))
+          .from(Tournament)
+          .where(inArray(Tournament.id, tournamentsToGet));
+      } catch (err) {
+        throw trpcUnknownError(err, 'Getting the tournaments');
+      }
+    }
+
+    if (usersToGet.length > 0) {
+      try {
+        users = await db
+          .select({
+            id: User.id,
+            username: OsuUser.username
+          })
+          .from(User)
+          .innerJoin(OsuUser, eq(OsuUser.osuUserId, User.osuUserId))
+          .where(inArray(User.id, usersToGet));
+      } catch (err) {
+        throw trpcUnknownError(err, 'Getting the users');
+      }
+    }
+
+    return {
+      notifications,
+      meta: {
+        tournaments,
+        users
+      }
+    };
+  });
 
 const markNotificationAsRead = t.procedure
   .input(wrap(v.object({ notificationId: positiveIntSchema })))
@@ -15,16 +113,11 @@ const markNotificationAsRead = t.procedure
 
     try {
       await db
-        .update(UserNotification)
+        .update(Notification)
         .set({
           read: true
         })
-        .where(
-          and(
-            eq(UserNotification.userId, session.userId),
-            eq(UserNotification.notificationId, notificationId)
-          )
-        );
+        .where(and(eq(Notification.userId, session.userId), eq(Notification.id, notificationId)));
     } catch (err) {
       throw trpcUnknownError(err, 'Marking the notification as read');
     }
@@ -35,17 +128,18 @@ const markAllNotificationsAsRead = t.procedure.mutation(async ({ ctx }) => {
 
   try {
     await db
-      .update(UserNotification)
+      .update(Notification)
       .set({
         read: true
       })
-      .where(and(eq(UserNotification.userId, session.userId), eq(UserNotification.read, false)));
+      .where(and(eq(Notification.userId, session.userId), eq(Notification.read, false)));
   } catch (err) {
     throw trpcUnknownError(err, "Marking all the user's notifications as read");
   }
 });
 
 export const notificationsRouter = t.router({
+  getNotifications,
   markNotificationAsRead,
   markAllNotificationsAsRead
 });
