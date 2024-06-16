bdcb557d72cd084655a2f816caa1b24901cba071
Mario564 <ka.mario564@gmail.com>
2024-06-16 14:02:02 -0700

    Update tournament procedures

--- src/lib/server/helpers/queries.ts
+++ src/lib/server/helpers/queries.ts
@@ -1,7 +1,8 @@
-import { db } from '$db';
+import { Notification, UserNotification, db } from '$db';
 import { count, sql } from 'drizzle-orm';
+import { pick } from '$lib/server/utils';
+import type { AnyPgTable, PgTransaction } from 'drizzle-orm/pg-core';
 import type { SQL } from 'drizzle-orm';
-import type { AnyPgTable } from 'drizzle-orm/pg-core';
 
 export async function getCount(table: AnyPgTable, where?: SQL) {
   return await db
@@ -13,4 +14,28 @@ export async function getCount(table: AnyPgTable, where?: SQL) {
 
 export async function setSimilarityThreshold() {
   return await db.execute(sql`set pg_trgm.similarity_threshold = 0.1`);
+}
+
+export async function createNotification(
+  message: string,
+  notifyUserIds: number[],
+  tx?: PgTransaction<any, any, any>
+) {
+  async function cb(tx: PgTransaction<any, any, any>) {
+    const notification = await tx
+      .insert(Notification)
+      .values({ message })
+      .returning(pick(Notification, ['id']))
+      .then((rows) => rows[0]);
+
+    await tx
+      .insert(UserNotification)
+      .values(notifyUserIds.map((userId) => ({ userId, notificationId: notification.id })));
+  }
+
+  if (tx) {
+    await cb(tx);
+  } else {
+    await db.transaction(cb);
+  }
 }
--- src/lib/server/procedures/tournaments.ts
+++ src/lib/server/procedures/tournaments.ts
@@ -14,7 +14,7 @@ import { wrap } from '@typeschema/valibot';
 import { getSession, getStaffMember, getTournament } from '../helpers/trpc';
 import { TRPCError } from '@trpc/server';
 import { hasPermissions, isDatePast, sortByKey } from '$lib/utils';
-import { eq, sql } from 'drizzle-orm';
+import { eq } from 'drizzle-orm';
 import {
   bwsValuesSchema,
   modMultiplierSchema,
@@ -436,21 +436,68 @@ const deleteTournament = t.procedure
     const tournament = await getTournament(
       tournamentId,
       {
+        tournament: ['deletedAt'],
         dates: ['concludesAt']
       },
       true
     );
     checks.tournamentNotConcluded(tournament);
 
+    if (tournament.deletedAt) {
+      throw new TRPCError({
+        code: 'FORBIDDEN',
+        message: 'This tournament is already scheduled to be deleted or it has already been deleted'
+      });
+    }
+
     try {
       await db
         .update(Tournament)
         .set({
-          deletedAt: sql`now()`
+          // Schedule to delete in the next 24 hours
+          deletedAt: new Date(new Date().getTime() + 86_400_000)
         })
         .where(eq(Tournament.id, tournamentId));
     } catch (err) {
-      throw trpcUnknownError(err, 'Deleting the tournament');
+      throw trpcUnknownError(err, "Scheduling the tournament's deletion");
+    }
+  });
+
+const cancelTournamentDeletion = t.procedure
+  .use(rateLimitMiddleware)
+  .input(
+    wrap(
+      v.object({
+        tournamentId: positiveIntSchema
+      })
+    )
+  )
+  .mutation(async ({ ctx, input }) => {
+    const { tournamentId } = input;
+    const checks = new TRPCChecks({ action: 'cancel the deletion of this tournament' });
+    const session = getSession(ctx.cookies, true);
+    const staffMember = await getStaffMember(session, tournamentId, true);
+    checks.staffHasPermissions(staffMember, ['host']);
+
+    const tournament = await getTournament(
+      tournamentId,
+      {
+        tournament: ['deletedAt'],
+        dates: ['concludesAt']
+      },
+      true
+    );
+    checks.tournamentNotConcluded(tournament).tournamentNotDeleted(tournament);
+
+    try {
+      await db
+        .update(Tournament)
+        .set({
+          deletedAt: null
+        })
+        .where(eq(Tournament.id, tournamentId));
+    } catch (err) {
+      throw trpcUnknownError(err, "Updating the tournament's deletion date");
     }
   });
 
@@ -458,5 +505,6 @@ export const tournamentsRouter = t.router({
   createTournament,
   updateTournament,
   updateTournamentDates,
-  deleteTournament
+  deleteTournament,
+  cancelTournamentDeletion
 });
