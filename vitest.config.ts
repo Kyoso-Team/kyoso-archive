08c0b9c40e1b8498218267b33e0f4ea7201bf331
Mario564 <ka.mario564@gmail.com>
2024-07-17 19:11:31 -0700

    Add sveltekit plugin to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,24 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-
-function lib(path?: string) {
-  const base = `${process.cwd()}/src/lib`;
-  return path ? `${base}/${path}` : base;
-}
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+  plugins: [sveltekit()],
   test: {
     include: ['**/tests/**/*.test.ts'],
-    exclude: ['**/tests/e2e/**'],
-    alias: {
-      $lib: lib(),
-      $stores: lib('stores'),
-      $types: lib('types'),
-      $components: lib('components'),
-      $trpc: lib('server/trpc'),
-      $db: lib('server/db'),
-      $tests: `${process.cwd()}/tests`
-    },
+    exclude: ['**/tests/components/**'],
     globalSetup: ['./tests/global-setup.ts']
   }
 });
