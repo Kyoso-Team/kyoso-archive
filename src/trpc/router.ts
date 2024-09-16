99d6bf62d0ff294dee912556b9e33a33079b0052
Mario564 <ka.mario564@gmail.com>
2024-09-16 14:14:39 -0700

    Move root router procedures

--- src/trpc/router.ts
+++ src/trpc/router.ts
@@ -1,14 +1,11 @@
 import { trpc } from '$lib/server/services';
-import { usersRouter } from './procedures/users';
-import { tournamentsRouter } from './procedures/tournaments';
-import { notificationsRouter } from './procedures/notifications';
-import { staffRolesRouter } from './procedures/staff-roles';
-import { search } from './procedures';
+import { usersRouter, tournamentsRouter, notificationsRouter, staffRolesRouter, formsRouter, rootProcedures } from './procedures';
 
 export const router = trpc.router({
   users: usersRouter,
   tournaments: tournamentsRouter,
   notifications: notificationsRouter,
   staffRoles: staffRolesRouter,
-  search
+  forms: formsRouter,
+  ...rootProcedures
 });
