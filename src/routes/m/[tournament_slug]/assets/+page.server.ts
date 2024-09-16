d2253b99f503f334dcfbe316ce9c46829aafa002
Mario564 <ka.mario564@gmail.com>
2024-09-15 20:45:50 -0700

    Rewrite a few checks

--- src/routes/m/[tournament_slug]/assets/+page.server.ts
+++ src/routes/m/[tournament_slug]/assets/+page.server.ts
@@ -1,15 +1,11 @@
-import { hasPermissions } from '$lib/utils';
-import { error } from '@sveltejs/kit';
 import { getTournament } from '$lib/server/context';
+import { checks } from '$lib/server/checks';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ parent, depends }) => {
   depends('reload:manage_assets');
   const { staffMember, tournament } = await parent();
-
-  if (!hasPermissions(staffMember, ['host', 'debug', 'manage_tournament', 'manage_assets'])) {
-    error(401, "You don't have the necessary permissions to access this page");
-  }
+  checks.page.staffHasPermissions(staffMember, ['host', 'debug', 'manage_tournament', 'manage_assets']);
 
   const assets = await getTournament(
     'page',
--- src/routes/m/[tournament_slug]/rules/+page.server.ts
+++ src/routes/m/[tournament_slug]/rules/+page.server.ts
@@ -1,18 +1,15 @@
-import { hasPermissions } from '$lib/utils';
 import { db } from '$lib/server/services';
 import { Tournament } from '$db';
 import { pick } from '$lib/server/utils';
 import { eq } from 'drizzle-orm';
-import { catcher, error } from '$lib/server/error';
+import { catcher } from '$lib/server/error';
+import { checks } from '$lib/server/checks';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ parent, depends }) => {
   depends('reload:manage_rules');
   const { staffMember, tournament } = await parent();
-
-  if (!hasPermissions(staffMember, ['host', 'debug', 'manage_tournament'])) {
-    error('page', 'unauthorized', "You don't have the necessary permissions to access this page");
-  }
+  checks.page.staffHasPermissions(staffMember, ['host', 'debug', 'manage_tournament']);
 
   const rules = await db
     .select(pick(Tournament, ['rules']))
